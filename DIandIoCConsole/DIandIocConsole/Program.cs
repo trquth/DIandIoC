22872f0627e25f89aa17624a8fe27070ac66ed60
trquth <thien.tran@saigontechnology.vn>
2016-12-16 10:24:56 +0700

    DI

--- DIandIoCConsole/DIandIocConsole/Program.cs
+++ DIandIoCConsole/DIandIocConsole/Program.cs
@@ -11,11 +11,14 @@ namespace DIandIocConsole
         static void Main(string[] args)
         {
             Console.WriteLine("------------DI and Ioc--------");
-            //IoC: Chuyen khoi tao Phone instance ra ngoai Persion
-            var phone = new Phone();
-            var user1 = new Person(phone);
+            //IoC: Chuyen khoi tao Phone instance ra ngoai Persion --> translating
+            //var phone = new Phone();
+            //DI
+            var stupidPhone = new StupidPhone();
+            var smartPhone = new SmartPhone();
+            var user1 = new Person(stupidPhone);
             user1.UsePhone();
-            var user2 = new Person(phone);
+            var user2 = new Person(smartPhone);
             user2.UsePhone();
             Console.ReadLine();
         }
@@ -23,8 +26,14 @@ namespace DIandIocConsole
     public class Person
     {
         //IoC :  Nhieu doi tuong cung dung chung 1 instance Phone ---> translating...
-        private readonly Phone _phone;
-        public Person(Phone phone)
+        //private readonly Phone _phone;
+        //public Person(Phone phone)
+        //{
+        //_phone = phone;
+        //}
+        //DI
+        private readonly IPhone _phone;
+        public Person(IPhone phone)
         {
             _phone = phone;
         }
@@ -35,12 +44,34 @@ namespace DIandIocConsole
             _phone.Call();
         }
     }
+    //Dependency Injection
+    public interface IPhone
+    {
+        void Call();
+    }
     public class Phone
     {
         private int count = 0;
         public void Call()
         {
             Console.WriteLine("Value {0}", count++);
+        }
+    }
+    //Implement IPhone
+    public class StupidPhone : IPhone
+    {
+        private int count = 0;
+        public void Call()
+        {
+            Console.WriteLine("Stupid Phone {0}", count++);
+        }
+    }
+    public class SmartPhone : IPhone
+    {
+        private int count = 0;
+        public void Call()
+        {
+            Console.WriteLine("Smart Phone {0}", count++);
         }
     }
 
